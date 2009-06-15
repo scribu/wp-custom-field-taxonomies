a1e98a711451d13357a26a9a5f08786060f44913
scribu <scribu@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-06-15 23:14:23 +0000

    CFT_query made static
    git-svn-id: http://plugins.svn.wordpress.org/custom-field-taxonomies/trunk@126271 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- query.php
+++ query.php
@@ -1,70 +1,85 @@
 <?php
 
-class CFT_query {
-	private $query_vars;
-	private $options;
-	private $filters;
-	private $penalties;
+class CFT_query 
+{
+	static $query_vars;
+	static $options;
+	private static $filters;
+	private static $penalties;
 
-	function __construct($query_vars, $options) {
-		$this->query_vars = $query_vars;
-		$this->options = $options;
+	static function init($query_vars, $options)
+	{
+		self::$query_vars = $query_vars;
+		self::$options = $options;
 
 		// Set filters		
-		$this->filters = array(
-			'posts_fields' => array($this, 'posts_fields'),
-			'posts_join' => array($this, 'posts_join'),
-			'posts_where' => array($this, 'posts_where'),
-			'posts_groupby' => array($this, 'posts_groupby'),
-			'posts_orderby' => array($this, 'posts_orderby'),
-			'the_posts' => array($this, 'rank_by_order'),
+		self::$filters = array(
+			'posts_fields' => '',
+			'posts_join' => '',
+			'posts_where' => '',
+			'posts_groupby' => '',
+			'posts_orderby' => '',
+			'the_posts' => 'rank_by_order',
 		);
 
 		// And add them
-		add_action('pre_get_posts', array($this, 'add_filters'));
+		add_action('pre_get_posts', array(__CLASS__ 'add_filters'));
 	}
 
-	// Adds filters only to main query
-	function add_filters($obj) {
+	static function add_filters($obj)
+	{
+		// Adds filters only to main query
 		if ( $GLOBALS['wp_query'] != $obj )
 			return;
-			
-		foreach ( $this->filters as $name => $callback )
-			add_filter($name, $callback);
+
+		foreach ( self::$filters as $name => $callback )
+		{
+			if ( empty($callback) )
+				$callback = $name;
+			add_filter($name, array(__CLASS__ $callback));
+		}
 	}
 
-	// Remove all filters, including this one
-	function remove_filters() {
-		foreach ( $this->filters as $name => $callback )
-			remove_filter($name, $callback);
+	static function remove_filters()
+	{
+		foreach ( self::$filters as $name => $callback )
+		{
+			if ( empty($callback) )
+				$callback = $name;
+			remove_filter($name, array(__CLASS__ $callback));
+		}
 	}
 
-	function posts_fields($fields) {
+	static function posts_fields($fields)
+	{
 		global $wpdb;
 
-		$nr = count($this->query_vars);
+		$nr = count(self::$query_vars);
 
 		return $fields . ", COUNT(*) * 100 / {$nr} AS meta_rank";
 	}
 
-	function posts_join($join) {
+	static function posts_join($join)
+	{
 		global $wpdb;
 
 		return $join . " JOIN {$wpdb->postmeta} ON ({$wpdb->posts}.ID = {$wpdb->postmeta}.post_id)";
 	}
 
-	function posts_where($where) {
+	static function posts_where($where)
+	{
 		global $wpdb;
 
-		if ( is_singular() ) {
-			$GLOBALS['CFT_core']->make_canonical();
+		if ( is_singular() )
+		{
+			CFT_core::make_canonical();
 
 			// Get posts instead of front page
 			$where = " AND {$wpdb->posts}.post_type = 'post' AND {$wpdb->posts}.post_status = 'publish'";
 		}
 
 		// Build CASE clauses
-		foreach ( $this->query_vars as $key => $value )
+		foreach ( self::$query_vars as $key => $value )
 			if ( empty($value) )
 				$clauses[] = $wpdb->prepare("WHEN '%s' THEN meta_value IS NOT NULL", $key);
 			elseif ( FALSE === strpos($value, '*') )
@@ -78,55 +93,62 @@ class CFT_query {
 		return $where . " AND CASE meta_key {$clauses} END";
 	}
 
-	function posts_groupby($groupby) {
+	static function posts_groupby($groupby)
+	{
 		global $wpdb;
 
 		// Set having
-		if ( $this->options['relevance'] )
+		if ( self::$options['relevance'] )
 			$having = ' HAVING COUNT(*) > 0';
 		else
-			$having = ' HAVING COUNT(*) = ' . count($this->query_vars);
+			$having = ' HAVING COUNT(*) = ' . count(self::$query_vars);
 
 		$column = "{$wpdb->posts}.ID";
 
 		// Add wp_posts.ID if it's not already added
-		if ( FALSE === strpos($groupby, $column) ) {
+		if ( FALSE === strpos($groupby, $column) )
+		{
 			if ( !empty($groupby) )
-				$column .=',';
+				$column .= ',';
 			$groupby = $column . $groupby;
 		}
 
 		return $groupby . $having;
 	}
 
-	function posts_orderby($orderby) {
+	static function posts_orderby($orderby)
+	{
 		return "meta_rank DESC, " . $orderby;
 	}
 
-	function rank_by_order($posts) {
-		$this->remove_filters();
+	// Sorts posts using penalties
+	static function rank_by_order($posts)
+	{
+		self::remove_filters();
 
-		if ( ! $this->options['rank_by_order'] )
+		if ( ! self::$options['rank_by_order'] )
 			return $posts;
 
-		$this->set_penalties();
+		self::set_penalties();
 		
-		foreach ( $posts as $post ) {
+		foreach ( $posts as $post )
+		{
 			// Get relevant keys
 			$values = get_post_custom($post->ID);
 
 			// Substract penalties
-			foreach ($this->query_vars as $key => $value)
+			foreach ( self::$query_vars as $key => $value )
 				if ( !@in_array($value, $values[$key]) )
-					$post->meta_rank -= $this->penalties[$key] / (count($this->query_vars) / 2);
+					$post->meta_rank -= self::$penalties[$key] / (count(self::$query_vars) / 2);
 		}
 
-		usort($posts, array($this, 'cmp_relevance'));
+		usort($posts, array(__CLASS__ 'cmp_relevance'));
 
 		return $posts;
 	}
 
-	function cmp_relevance($postA, $postB) {
+	static function cmp_relevance($postA, $postB)
+	{
 		$a = $postA->meta_rank;
 		$b = $postB->meta_rank;
 
@@ -137,19 +159,23 @@ class CFT_query {
 	}
 
 	// Penalties are based on the order of the query vars
-	function set_penalties() {
-		foreach ( array_keys($this->query_vars) as $key ) {
-			$this->penalties[$key] = strpos($_SERVER['QUERY_STRING'], $key.'=');
-			$this->penalties[$key] = count($this->penalties) - 1;
+	static function set_penalties()
+	{
+		foreach ( array_keys(self::$query_vars) as $key )
+		{
+			self::$penalties[$key] = strpos($_SERVER['QUERY_STRING'], $key.'=');
+			self::$penalties[$key] = count(self::$penalties) - 1;
 		}
 
-		$values = array_values($this->penalties);
+		$values = array_values(self::$penalties);
 		rsort($values);
 
-		$this->penalties = array_combine(array_keys($this->penalties), $values);
+		self::$penalties = array_combine(array_keys(self::$penalties), $values);
 	}
 
-	function meta_relevance($echo) {
+	// Template tag
+	static function meta_relevance($echo)
+	{
 		global $post;
 
 		$relevance = round($post->meta_rank) . '%';
