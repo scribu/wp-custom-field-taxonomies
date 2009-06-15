0ad8b2797cda4e399dead6b76b15b0be8b353c1e
scribu <scribu@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-06-15 22:57:44 +0000

    CFT_core made static
    git-svn-id: http://plugins.svn.wordpress.org/custom-field-taxonomies/trunk@126270 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- main.php
+++ main.php
@@ -24,72 +24,83 @@ You should have received a copy of the GNU General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 
+require_once dirname(__FILE__) . '/inc/scb-check.php';
+if ( ! scb_check(__FILE__) ) return;
+
 define('CFT_AJAX_KEY', 'ajax-meta-search');
 define('CFT_AJAX_URL', get_bloginfo('url') . '?' . CFT_AJAX_KEY . '=');
 define('CFT_AJAX_URL_JS', "<script type='text/javascript'>window.cft_suggest_url = '" . CFT_AJAX_URL . "';</script>");
 
-class CFT_core {
-	public $map;
-	public $query_vars;
+abstract class CFT_core
+{
+	static $options;
+	static $map;
+	static $query_vars;
 
 // Core methods
 
-	function __construct($options) {
-		$this->options = $options;
+	static function init($options)
+	{
+		self::$options = $options;
 
 		// Generate map with the latest settings
-		$this->make_map();
+		self::make_map();
 
 		// Detect query
-		if ( ! $this->detect_query() )
+		if ( ! self::detect_query() )
 			return false;
 
-		require_once(dirname(__FILE__) . '/query.php');
-		$GLOBALS['CFT_query'] = new CFT_query($this->query_vars, $this->options->get());
+		require_once dirname(__FILE__) . '/query.php';
+		CFT_query::init(self::$query_vars, self::$options->get());
 
 		// Set ajax response
-		add_action('init', array($this, 'ajax_meta_search'));
+		add_action('init', array(__CLASS__ 'ajax_meta_search'));
 
 		// Customize template and title
-		add_action('template_redirect', array($this, 'add_template'), 999);
-		add_filter('wp_title', array($this, 'set_title'), 20, 3);
+		add_action('template_redirect', array(__CLASS__ 'add_template'), 999);
+		add_filter('wp_title', array(__CLASS__ 'set_title'), 20, 3);
 	}
 
-	function make_map() {
-		$this->map = (array) $this->options->get('map');
+	static function make_map()
+	{
+		self::$map = (array) self::$options->get('map');
 
-		foreach ( $this->map as $key => $name )
+		foreach ( self::$map as $key => $name )
 			if ( empty($name) )
-				$this->map[$key] = ucfirst($key);
+				self::$map[$key] = ucfirst($key);
 
 		// for convenience,
-		return $this->map;
+		return self::$map;
 	}
 
-	private function detect_query() {
-		if ( is_admin() || empty($_GET) || empty($this->map) )
+	private static function detect_query()
+	{
+		if ( is_admin() || empty($_GET) || empty(self::$map) )
 			return false;
 
-		$keys = array_keys($this->map);
+		$keys = array_keys(self::$map);
 
 		foreach ( $_GET as $key => $value )
 			if ( in_array($key, $keys) )
-				$this->query_vars[$key] = wp_specialchars($value);
+				self::$query_vars[$key] = wp_specialchars($value);
 				
-		return ! empty($this->query_vars);
+		return ! empty(self::$query_vars);
 	}
 
-	function add_template() {
+	static function add_template()
+	{
 		$template = TEMPLATEPATH . "/meta.php";
 
-		if ( file_exists($template) ) {
+		if ( file_exists($template) )
+		{
 			include($template);
 			die;
 		}
 	}
 
-	function set_title($title, $sep, $seplocation = '') {
-		$newtitle[] = $this->get_meta_title();
+	static function set_title($title, $sep, $seplocation = '')
+	{
+		$newtitle[] = self::get_meta_title();
 		$newtitle[] = " $sep ";
 
 		if ( ! empty($title) )
@@ -103,9 +114,11 @@ class CFT_core {
 
 // Template tags
 
-	function get_meta_title($format = '%name%: %value%', $between = ' and ') {
-		foreach ( $this->query_vars as $key => $value ) {
-			$name = $this->map[$key];
+	static function get_meta_title($format = '%name%: %value%', $between = ' and ')
+	{
+		foreach ( self::$query_vars as $key => $value )
+		{
+			$name = self::$map[$key];
 
 			$title[] = str_replace(array('%name%', '%value%'), array($name, stripslashes($value)), $format);
 		}
@@ -113,8 +126,9 @@ class CFT_core {
 		return implode($between, $title);
 	}
 
-	function get_linked_meta($id, $key, $glue = ', ', $relative = false) {
-		if ( ! $this->is_defined($key) )
+	static function get_linked_meta($id, $key, $glue = ', ', $relative = false)
+	{
+		if ( ! self::is_defined($key) )
 			return false;
 
 		$values = get_post_meta($id, $key);
@@ -123,7 +137,7 @@ class CFT_core {
 			return false;
 
 		foreach ( $values as $i => $value )
-			$values[$i] = sprintf('<a href="%s">%s</a>', $this->get_meta_url($key, $value, $relative), $value);
+			$values[$i] = sprintf('<a href="%s">%s</a>', self::get_meta_url($key, $value, $relative), $value);
 
 		if ( count($values) > 1 )
 			$content = implode($glue, $values);
@@ -133,14 +147,15 @@ class CFT_core {
 		return apply_filters('get_linked_meta', $content, $id, $key, $glue);
 	}
 
-	function meta_cloud($metaArgs, $cloudArgs = '') {
+	static function meta_cloud($metaArgs, $cloudArgs = '')
+	{
 		extract(wp_parse_args($metaArgs, array(
 			'key' => NULL,
 			'auth_id' => NULL,
 			'relative' => false
 		)));
 
-		if ( ! $this->is_defined($key) )
+		if ( ! self::is_defined($key) )
 			return false;
 
 		$cloudArgs = wp_parse_args($cloudArgs, array(
@@ -149,14 +164,14 @@ class CFT_core {
 			'exclude' => '', 'include' => '', 'link' => 'view'
 		));
 
-		$tags = $this->get_meta_values($key, $auth_id, $cloudArgs['number']);
+		$tags = self::get_meta_values($key, $auth_id, $cloudArgs['number']);
 
 		if ( empty($tags) )
 			return false;
 
 		// Add links
 		foreach ( $tags as $i => $tag )
-			$tags[$i]->link = $this->get_meta_url($key, $tag->name, $relative);
+			$tags[$i]->link = self::get_meta_url($key, $tag->name, $relative);
 
 		$return = wp_generate_tag_cloud($tags, $cloudArgs);
 
@@ -166,12 +181,13 @@ class CFT_core {
 		echo $return;
 	}
 
-	function filter_box($exclude = array() ) {
-		add_action('wp_footer', array($this, 'filter_box_scripts'));
+	static function filter_box($exclude = array() )
+	{
+		add_action('wp_footer', array(__CLASS__ 'filter_box_scripts'));
 
 		// Generate select
 		$select = '<option />';
-		foreach ( $this->map as $key => $name )
+		foreach ( self::$map as $key => $name )
 			if ( ! in_array($key, $exclude) )
 				$select .= sprintf('<option value="%s">%s</option>', $key, $name);
 		$select = "<select>{$select}</select>\n";
@@ -191,10 +207,11 @@ class CFT_core {
 
 // Helper methods
 
-	function filter_box_scripts() {
+	static function filter_box_scripts()
+	{
 		global $wp_scripts;
 
-		$url = $this->get_plugin_url() . '/inc';
+		$url = self::get_plugin_url() . '/inc';
 		$scriptf = "<script language='javascript' type='text/javascript' src='%s'></script>";
 
 		// CSS
@@ -212,15 +229,17 @@ class CFT_core {
 		echo implode("\n", $scripts);
 	}
 
-	private function is_defined($key) {
-		if ( ! $r = in_array($key, array_keys($this->map)) )
+	private static function is_defined($key)
+	{
+		if ( ! $r = in_array($key, array_keys(self::$map)) )
 			trigger_error("Undefined meta taxonomy: $key", E_USER_WARNING);
 
 		return $r;
 	}
 
 	// AJAX response
-	function ajax_meta_search() {
+	static function ajax_meta_search()
+	{
 		if ( !isset( $_GET[CFT_AJAX_KEY] ) )
 			return;
 
@@ -229,18 +248,19 @@ class CFT_core {
 		$key = $wpdb->escape(trim($_GET[CFT_AJAX_KEY]));
 		$hint = $wpdb->escape(trim($_GET['q']));
 
-		if ( ! $this->is_defined($key) )
+		if ( ! self::is_defined($key) )
 			die(-1);
 
 		@header('Content-Type: text/html; charset=' . get_option('blog_charset'));
 
-		foreach ( $this->get_meta_values($key, NULL, 10, $hint) as $value )
+		foreach ( self::get_meta_values($key, NULL, 10, $hint) as $value )
 			echo $value->name . "\n";
 
 		die;
 	}
 
-	private function get_meta_values($key, $auth_id = NULL, $limit = NULL, $hint = NULL) {
+	private static function get_meta_values($key, $auth_id = NULL, $limit = NULL, $hint = NULL)
+	{
 		global $wpdb;
 
 		$where = "AND m.meta_key = '$key'";
@@ -268,12 +288,13 @@ class CFT_core {
 		return apply_filters('cft_get_values', $values, $key, $auth_id, $limit, $hint);
 	}
 
-	private function get_meta_url($key, $value, $relative = false) {
+	private static function get_meta_url($key, $value, $relative = false)
+	{
 		if ( is_singular() )
 			$relative = false;
 
 		if ( $relative )
-			$url = $this->get_current_url();
+			$url = self::get_current_url();
 		else
 			$url = trailingslashit(get_bloginfo('url'));
 
@@ -282,21 +303,23 @@ class CFT_core {
 		return apply_filters('cft_get_url', $url, $key, $value, $relative);
 	}
 
-	function make_canonical() {
+	static function make_canonical()
+	{
 		// Get canonical location (shouldn't be relative for single posts)
 		$location = trailingslashit(get_bloginfo('url'));
 
-		foreach ( $this->query_vars as $key => $value )
+		foreach ( self::$query_vars as $key => $value )
 			$location = add_query_arg($key, urlencode($value), $location);
 
-		if ( $this->get_current_url() == $location )
+		if ( self::get_current_url() == $location )
 			return; // all good
 
 		wp_redirect($location, 301);
 		die;
 	}
 
-	private function get_current_url() {
+	private static function get_current_url()
+	{
 		$pageURL = ($_SERVER["HTTPS"] == "on") ? 'https://' : 'http://';
 
 		if ( $_SERVER["SERVER_PORT"] != "80" )
@@ -307,7 +330,8 @@ class CFT_core {
 		return $pageURL;
 	}
 
-	private function get_plugin_url() {
+	private static function get_plugin_url()
+	{
 		// < WP 2.6
 		if ( !function_exists('plugins_url') )
 			return get_option('siteurl') . '/wp-content/plugins/' . plugin_basename(dirname(__FILE__));
@@ -317,9 +341,12 @@ class CFT_core {
 }
 
 // Init
-function cft_init() {
+
+cft_init();
+function cft_init()
+{
 	// Load scbFramework
-	require_once(dirname(__FILE__) . '/inc/scb/load.php');
+//	require_once(dirname(__FILE__) . '/inc/scb/load.php');
 
 	$options = new scbOptions('cf_taxonomies', __FILE__, array(
 		'map' => '',
@@ -327,19 +354,18 @@ function cft_init() {
 		'rank_by_order' => false
 	));
 
-	$GLOBALS['CFT_core'] = new CFT_core($options);
+	CFT_core::init($options);
 
-	include_once(dirname(__FILE__) . '/template-tags.php');
+	include_once dirname(__FILE__) . '/template-tags.php';
 
-	if ( is_admin() ) {
-		require_once(dirname(__FILE__) . '/admin.php');
-		new settingsCFT(__FILE__, $options, $GLOBALS['CFT_core']->make_map());
+	if ( is_admin() )
+	{
+		require_once dirname(__FILE__) . '/admin.php';
+		new settingsCFT(__FILE__, $options, CFT_core::make_map());
 	}
-	
+
 	// DEBUG
 	if ( CFT_DEBUG === true )
 		add_action('wp_footer', create_function('', 'print_r($GLOBALS["wp_query"]->request);'));
 }
-
-cft_init();
 
