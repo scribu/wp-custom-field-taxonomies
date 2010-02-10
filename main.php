ee40d6ad044ab83ac3540f9dd0d61e5320f2d12e
scribu <scribu@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-02-10 15:23:49 +0000

    re-add get_current_url()
    git-svn-id: http://plugins.svn.wordpress.org/custom-field-taxonomies/trunk@203719 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- main.php
+++ main.php
@@ -265,6 +265,17 @@ if ( !empty($old_map) )
 
 		return apply_filters('cft_get_url', $url, $key, $value, $relative);
 	}
+	
+	private static function get_current_url() {
+		$pageURL = ($_SERVER["HTTPS"] == "on") ? 'https://' : 'http://';
+
+		if ( $_SERVER["SERVER_PORT"] != "80" )
+			$pageURL .= $_SERVER["SERVER_NAME"]. ":" .$_SERVER["SERVER_PORT"] . $_SERVER["REQUEST_URI"];
+		else
+			$pageURL .= $_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"];
+
+		return $pageURL;
+	}
 
 	private static function terms_clause($str) {
 		if ( empty($str) )
